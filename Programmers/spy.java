40f5b7f9e8ee99f8372d71388080d6482e60cd85
jeonghoo <austinan@naver.com>
2023-01-09 22:46:52 +0900

    spy

--- Programmers/spy.java
+++ Programmers/spy.java
@@ -1,12 +1,26 @@
 package Programmers;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class spy {
 
     public int solution(String [][] clothes) {
 
         // 옷을 종류별로 구분
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String [] clothe : clothes) {
+            String type = clothe[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
 
+        // 입지 않는 경우 고려
+        Iterator<Integer> it = map.values().iterator();
+        int answer = 1;
 
+        while(it.hasNext()) answer *= it.next().intValue() + 1;
+
+        return answer - 1;
     }
 
 }
