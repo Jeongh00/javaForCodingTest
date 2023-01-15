975142192a60c2994a773fff5aa8f09de7e95732
안정후 <austinan@naver.com>
2023-01-15 18:37:31 +0900

    FindSum

---
+++ lecture/FindSum.java
@@ -0,0 +1,16 @@
+package lecture;
+
+public class FindSum {
+    public int solution(int n, int m, int [] arr) {
+        int answer=0, sum=0, lt=0;
+        for(int rt=0; rt<n; rt++) {
+            sum += arr[rt];
+            if(sum == m) answer++;
+            while(sum >= m) {
+                sum -= arr[lt++];
+                if(sum == m) answer++;
+            }
+        }
+        return answer;
+    }
+}
