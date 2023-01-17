7022031147271d91421be6deff092e2e791ab3a4
안정후 <austinan@naver.com>
2023-01-17 22:54:28 +0900

    correctBracket

--- lecture/StackAndQueue/correctBracket.java
+++ lecture/StackAndQueue/correctBracket.java
@@ -1,5 +1,25 @@
 package lecture.StackAndQueue;
 
+import java.util.Stack;
+
 public class correctBracket {
+
+    public String solution(String str){
+
+        String answer = "YES";
+
+        Stack<Character> stack = new Stack<>();
+        for(char x : str.toCharArray()){
+            if(x == '(') stack.push(x);
+            else{
+                if(stack.isEmpty()) return "NO";
+                stack.pop();
+            }
+        }
+
+        if(!stack.isEmpty()) return "NO";
+        return answer;
+
+    }
 
 }
